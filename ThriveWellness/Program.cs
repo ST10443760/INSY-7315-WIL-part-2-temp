c5ba51a37e7e70c30674c92e4371d9d7f0e811a7
muaazabdool-gaffoor <ST10443760@vcconnect.edu.za>
2026-09-20 18:42:29 +0200

    Register ScheduledNotificationHostedService

--- ThriveWellness/Program.cs
+++ ThriveWellness/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddScoped<IPaymentService>(sp =>
 });
 builder.Services.AddScoped<INotificationService, NotificationService>();
 builder.Services.AddScoped<IScheduledNotificationService, ScheduledNotificationService>();
+builder.Services.AddHostedService<ScheduledNotificationHostedService>();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
