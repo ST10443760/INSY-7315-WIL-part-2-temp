c95b0ce9337bc4dfe2e15c371d3ae4fe4352d760
muaazabdool-gaffoor <ST10443760@vcconnect.edu.za>
2026-09-20 17:22:09 +0200

    Add IPaymentRepository.CreateAsync

---
+++ ThriveWellness/Repositories/Implementations/PaymentRepository.cs
@@ -0,0 +1,22 @@
+using ThriveWellness.Data;
+using ThriveWellness.Models;
+using ThriveWellness.Repositories.Interfaces;
+
+namespace ThriveWellness.Repositories.Implementations
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(Payment payment)
+        {
+            _context.Payments.Add(payment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- ThriveWellness/Repositories/Interfaces/IPaymentRepository.cs
+++ ThriveWellness/Repositories/Interfaces/IPaymentRepository.cs
@@ -1,6 +1,9 @@
+using ThriveWellness.Models;
+
 namespace ThriveWellness.Repositories.Interfaces
 {
     public interface IPaymentRepository
     {
+        Task CreateAsync(Payment payment);
     }
 }
